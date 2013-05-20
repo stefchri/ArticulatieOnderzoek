324f6b22922c5efa050dd7b66f0b8e8ed3d1b301
Stefaan Christiaens <stefaan.ch@gmail.com>
2013-05-20 11:17:00 +0200

    fail git commit

---
+++ LibAOModels/Result.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibAOModels
+{
+    public class Result
+    {
+        public Int64 ID { get; set; }
+        [Required]
+        public Int32 Order { get; set; }
+        public string AudioSource { get; set; }
+        public Int32 Phonetic { get; set; }
+        public Int16 Value { get; set; }
+        public Int64 TestID { get; set; }
+
+        public virtual Test Test { get; set; }
+        public virtual ICollection<Error> Errors { get; set; }
+    }
+}
